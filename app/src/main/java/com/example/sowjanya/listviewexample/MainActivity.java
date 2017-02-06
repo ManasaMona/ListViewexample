cd393099b6d395d37454087338fbbdfa28ec34d3
ManasaMona <manasaalisetty@gmail.com>
2017-02-06 10:28:57 +0530

    version 1

---
+++ app/src/main/java/com/example/sowjanya/listviewexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sowjanya.listviewexample;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainListView = (ListView) findViewById( R.id.mainListView );
+
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars","Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+
+        mainListView.setAdapter( listAdapter );
+
+    }
+}
